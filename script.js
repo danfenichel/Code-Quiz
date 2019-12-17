14aa356e3fde48505331190e9363c90bd17c997e
D0491 <dan.fenichel@yahoo.com>
2019-12-16 20:13:01 -0600

    wrote out all quiz questions and answers

---
+++ script.js
@@ -0,0 +1,54 @@
+// DOM elements
+
+// Questions
+var questions = [
+    {
+        question: "What is the world's largest island?",
+        choices: ["Hawaii", "Iceland", "Greenland", "Austrailia"],
+        answer: "Greenland"
+    },
+    {
+        question: "What is the world's longest river?",
+        choices: ["Amazon River", "Nile River", "Colorado River", "Mississippi River"],
+        answer: "Amazon River"
+    },
+    {
+        question: "What is the world's largest ocean?",
+        choices: ["Atlantic Ocean", "Pacific Ocean", "Indian Ocean", "Arctic Ocean"],
+        answer: "Pacific Ocean"
+    },
+    {
+        question: "What is the diameter of the Earth?",
+        choices: ["8,000 miles", "24,000 miles", "16,000 miles", "1,000 miles"],
+        answer: "8,000 miles"
+    },
+    {
+        question: "What is the world's most populous country?",
+        choices: ["United States", "India", "Russia", "China"],
+        answer: "China"
+    }
+];
+
+// Event listeners
+
+    // Start quiz
+
+    // Click answers (send to next question)
+
+    // Submit initials
+
+    // Clear high scores
+
+    // Start quiz over
+
+// Functions
+
+    // Start quiz
+
+    // Log correct/incorrect answers
+
+    // Cycle through questions
+
+    // Final score
+
+    // Store high scores
